195bd1c480d627ebaef0c1391aff8ea20e72b58b
skyofgamer <87489188+skyofgamer@users.noreply.github.com>
2023-09-29 19:43:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(600, 600);
+canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+save("myFilterImage.png");
+}
